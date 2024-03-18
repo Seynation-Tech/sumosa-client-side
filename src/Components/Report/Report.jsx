8226e300160c7d89edb015031c508e6e2f00bd7f
scottdath <sciencedath@gmail.com>
2024-03-18 12:24:49 +0300

    API Intergration , to AUTH almost done, not yet to other areas

--- src/Components/Report/Report.jsx
+++ src/Components/Report/Report.jsx
@@ -27,7 +27,7 @@ import DotLoader from "react-spinners/DotLoader";
 import Cookies from "cookie-universal";
 
 let today = new Date()
-let  date =  today.getDate()  + '/' +  (today.getMonth() + 1)  + '/' + today.getFullYear();
+let  date =  today.getDate()  + '/' +  (today.getMonth() + 1)  + '/' + today.getFullYear() +' ' + today.getHours() + ':' + today.getMinutes();
 let  mydate =  today.getDate()  + '/' +  (today.getMonth() + 1)  + '/' + today.getFullYear() + ' ' + today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 
 
